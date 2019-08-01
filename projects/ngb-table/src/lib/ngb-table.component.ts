57186bd38d6795ce3858dba0ab5a8fc2ebb4f70b
Michal Jaworski <mikejav@users.noreply.github.com>
2019-08-01 18:37:44 +0200

    improve import paths

--- projects/ngb-table/src/lib/ngb-table.component.ts
+++ projects/ngb-table/src/lib/ngb-table.component.ts
@@ -1,11 +1,11 @@
-import { NgbTableSelectActionsHeaderDirective } from './children-selectors/ngb-table-select-actions-header.directive';
 import { Component, OnInit, ContentChild, TemplateRef, ContentChildren, QueryList, Input, Output, EventEmitter } from '@angular/core';
 
-import { NgbTableHeaderDirective } from 'projects/ngb-table/src/lib/children-selectors/ngb-table-header.directive';
-import { NgbTableHeadDirective } from 'projects/ngb-table/src/lib/children-selectors/ngb-table-head.directive';
-import { NgbTableBodyRowDirective } from 'projects/ngb-table/src/lib/children-selectors/ngb-table-body-row.directive';
-import { NgbTableFooterRowDirective } from 'projects/ngb-table/src/lib/children-selectors/ngb-table-footer-row.directive';
-import { NgbTableFooterDirective } from 'projects/ngb-table/src/lib/children-selectors/ngb-table-footer.directive';
+import { NgbTableHeaderDirective } from './children-selectors/ngb-table-header.directive';
+import { NgbTableSelectActionsHeaderDirective } from './children-selectors/ngb-table-select-actions-header.directive';
+import { NgbTableHeadDirective } from './children-selectors/ngb-table-head.directive';
+import { NgbTableBodyRowDirective } from './children-selectors/ngb-table-body-row.directive';
+import { NgbTableFooterRowDirective } from './children-selectors/ngb-table-footer-row.directive';
+import { NgbTableFooterDirective } from './children-selectors/ngb-table-footer.directive';
 
 
 type Row = object;
